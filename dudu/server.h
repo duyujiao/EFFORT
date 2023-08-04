396d21fc1e3f152d737c37dd87553d4cf40b8034
duyujiao <2637599327@qq.com>
2023-08-04 21:50:04 +0800

    server class finished

---
+++ dudu/server.h
@@ -0,0 +1,42 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+using namespace std;
+class server
+{
+    private:
+    //服务器端口号
+    int server_port;
+    //设为listen状态的套接字描述符
+    int server_sockfd;
+    //服务器ip
+    string server_ip;
+    //保存所有的套接字描述符
+    vector<int> sock_arr;
+    public:
+    //构造函数
+    server(int port,string ip);
+    //析构函数
+    ~server();
+    //服务器开始服务
+    void run();
+    //子线程工作的静态函数
+    static void RecvMsg(int conn);
+};
+
+
+
+#endif
