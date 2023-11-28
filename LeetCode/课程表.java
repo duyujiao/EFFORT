ad3d477dcc63d2ebfe6e2919a8cdc1201bd6990b
duyujiao <2637599327@qq.com>
2023-11-28 20:40:00 +0800

    拓扑排序课程表

--- LeetCode/课程表.java
+++ LeetCode/课程表.java
@@ -1,29 +1,44 @@
-
 class Solution {
-    public boolean isPalindrome(String s) {
-        String str = s.toLowerCase();
-        char[] ss = str.toCharArray();
-        int l = str.length();
-        int left = 0;
-        int right = l - 1;
-
-        // 验证回文串
-        while (left < right) {
-            while (left < right && !Character.isLetterOrDigit(ss[left])) {
-                left++;
-            }
-            while (left < right && !Character.isLetterOrDigit(ss[right])) {
-                right--;
-            }
-
-            if (ss[left] != ss[right]) {
-                return false;
-            }
-            
-            left++;
-            right--;
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> list=new ArrayList<>();
+        int count=0;
+        //初始化
+        for(int i=0;i<numCourses;i++)
+        {
+            list.add(new ArrayList());
         }
-
+        int[] indegree=new int[numCourses];
+        //构建邻接表
+        for(int[] e:prerequisites)
+        {
+            int b=e[0];
+            int a=e[1];
+            list.get(a).add(b);
+            indegree[b]++;
+        }
+        Stack<Integer> stack=new Stack<>();
+        //入度为0的点入栈
+        for(int i=0;i<numCourses;i++)
+        {
+            if(indegree[i]==0)
+            stack.push(i);
+        }
+        //更新indegree数组
+        while(!stack.isEmpty())
+        {
+            int top=stack.pop();
+            count++;
+            for(int n:list.get(top))
+            {
+            indegree[n]--;
+            if(indegree[n]==0)
+            stack.push(n);
+            }
+        }
+        if(count==numCourses)
         return true;
+        else
+        return false;
+
     }
 }
