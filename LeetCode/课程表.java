d480bcd5aeeddc6930f79bfdf7e36cf66977c5fd
duyujiao <2637599327@qq.com>
2023-11-28 20:39:01 +0800

    拓扑排序课程表

---
+++ LeetCode/课程表.java
@@ -0,0 +1,29 @@
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.toLowerCase();
+        char[] ss = str.toCharArray();
+        int l = str.length();
+        int left = 0;
+        int right = l - 1;
+
+        // 验证回文串
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(ss[left])) {
+                left++;
+            }
+            while (left < right && !Character.isLetterOrDigit(ss[right])) {
+                right--;
+            }
+
+            if (ss[left] != ss[right]) {
+                return false;
+            }
+            
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
