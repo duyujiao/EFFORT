ac67d8809548b1da5e8c118d74bb5afba69dada2
duyujiao <2637599327@qq.com>
2023-11-29 20:55:11 +0800

    字符串反转

---
+++ LeetCode/字符串反转.c
@@ -0,0 +1,18 @@
+void reverseString(char* s, int sSize) {
+    int p;
+    int q;
+    int m=sSize;
+    int n=0;
+    for(int i=0;i<sSize/2;i++)
+    {
+
+        p=n;
+        q=m-1;
+        char t;
+        t=s[p];
+        s[p]=s[q];
+        s[q]=t;
+        n++;
+        m--;
+    }
+}
